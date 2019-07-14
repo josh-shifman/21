483b1e2659b93388e2c6c841cd3d2c0615b046e3
Servillian <47287191+Servillian@users.noreply.github.com>
2019-07-14 12:26:26 +1000

    whitejill

--- main.py
+++ main.py
@@ -10,16 +10,65 @@ class Card:
     def __radd__(self, other):
         return other + self.rank
 
-def randomCard():
 
+def randomCard():
     return Card(randint(1, 10))
+
+def play_again():
+    again = input("Do you want to play again? \n")
+    if again == "Yes" or again == "yes":
+        main()
+    elif again == "No" or again == "no":
+        exit()
 
 def main():
     player1 = []
     player2 = []
-    for i in range(3):
-        player1.append(randomCard())
-    print(sum(player1), print(player1))
+    game = 0
+    turn = True
+    while game < 2:
+        while turn == True:
+            a = input("Player 1's turn: \n")
+            if a == "hit me":
+                player1.append(randomCard())
+                print("Player 1's cards:", player1,"\n")
+                turn = False
+                game = 0
+            elif a == "pass":
+                turn = False
+                game +=1
+        while turn == False:
+            b = input("Player 2's turn: \n")
+            if b == "hit me":
+                player2.append(randomCard())
+                print("Player 2's cards:", player2,"\n")
+                turn = True
+            elif b == "pass":
+                turn = True
+                game+=1
+
+    player1sum = sum(player1)
+    player2sum = sum(player2)
+    print("\nPlayer 1:", player1sum, "\nPlayer 2:", player2sum)
+
+    if (player1sum <= 21) and (player2sum > 21):
+        print("Player 1 Wins")
+    elif (player1sum > 21) and (player2sum <= 21):
+        print("Player 2 Wins")
+    elif player1sum == player2sum:
+        print("It's a tie")
+    elif player1sum == 21 and player2sum != 21:
+        print("Player 1 Wins")
+    elif player1sum != 21 and player2sum == 21:
+        print("Player 2 Wins")
+    elif player1sum > player2sum:
+        print("Player 1 Wins")
+    elif player2sum > player1sum:
+        print("Player 2 Wins")
+
+
+
+
 
 
 
